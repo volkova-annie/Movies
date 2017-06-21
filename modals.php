6a51ef16f3830f2d34b24083a348e821c1d8ece7
Anna Volkova <annie_v@bk.ru>
2017-06-21 20:19:34 +0300

    modals

---
+++ modals.php
@@ -0,0 +1,190 @@
+<!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Choose seats</h4>
+      </div>
+      <div class="modal-body">
+          <div class="hall">
+            <div class="row text-center">
+            <div class="row text-center seat-view">
+              <button class="hall-seat-view-green"  disabled="disabled"></button> chosen seats
+              <button class="hall-seat-view-pink"  disabled="disabled"></button> occupied seats
+              <button class="hall-seat-view-gray"  disabled="disabled"></button> available seats
+            </div>
+              1 row
+              <button class="hall-seat" data-row="1" data-seat="1">1</button>
+              <button class="hall-seat" data-row="1" data-seat="2">2</button>
+              <button class="hall-seat" data-row="1" data-seat="3">3</button>
+              <button class="hall-seat" data-row="1" data-seat="4">4</button>
+              <button class="hall-seat" data-row="1" data-seat="5">5</button>
+              <button class="hall-seat" data-row="1" data-seat="6">6</button>
+              <button class="hall-seat" data-row="1" data-seat="7">7</button>
+              <button class="hall-seat" data-row="1" data-seat="8">8</button>
+              <button class="hall-seat" data-row="1" data-seat="9">9</button>
+              <button class="hall-seat" data-row="1" data-seat="10">10</button>
+              1 row
+            </div>
+            <div class="row text-center">
+              2 row
+              <button class="hall-seat" data-row="2" data-seat="1">1</button>
+              <button class="hall-seat" data-row="2" data-seat="2">2</button>
+              <button class="hall-seat" data-row="2" data-seat="3">3</button>
+              <button class="hall-seat" data-row="2" data-seat="4">4</button>
+              <button class="hall-seat" data-row="2" data-seat="5">5</button>
+              <button class="hall-seat" data-row="2" data-seat="6">6</button>
+              <button class="hall-seat" data-row="2" data-seat="7">7</button>
+              <button class="hall-seat" data-row="2" data-seat="8">8</button>
+              <button class="hall-seat" data-row="2" data-seat="9">9</button>
+              <button class="hall-seat" data-row="2" data-seat="10">10</button>
+              2 row
+            </div>
+            <div class="row text-center">
+              3 row
+              <button class="hall-seat" data-row="3" data-seat="1">1</button>
+              <button class="hall-seat" data-row="3" data-seat="2">2</button>
+              <button class="hall-seat" data-row="3" data-seat="3">3</button>
+              <button class="hall-seat" data-row="3" data-seat="4">4</button>
+              <button class="hall-seat" data-row="3" data-seat="5">5</button>
+              <button class="hall-seat" data-row="3" data-seat="6">6</button>
+              <button class="hall-seat" data-row="3" data-seat="7">7</button>
+              <button class="hall-seat" data-row="3" data-seat="8">8</button>
+              <button class="hall-seat" data-row="3" data-seat="9">9</button>
+              <button class="hall-seat" data-row="3" data-seat="10">10</button>
+              3 row
+            </div>
+            <div class="row text-center">
+              4 row
+              <button class="hall-seat" data-row="4" data-seat="1">1</button>
+              <button class="hall-seat" data-row="4" data-seat="2">2</button>
+              <button class="hall-seat" data-row="4" data-seat="3">3</button>
+              <button class="hall-seat" data-row="4" data-seat="4">4</button>
+              <button class="hall-seat" data-row="4" data-seat="5">5</button>
+              <button class="hall-seat" data-row="4" data-seat="6">6</button>
+              <button class="hall-seat" data-row="4" data-seat="7">7</button>
+              <button class="hall-seat" data-row="4" data-seat="8">8</button>
+              <button class="hall-seat" data-row="4" data-seat="9">9</button>
+              <button class="hall-seat" data-row="4" data-seat="10">10</button>
+              4 row
+            </div>
+            <div class="row text-center">
+              5 row
+              <button class="hall-seat" data-row="5" data-seat="1">1</button>
+              <button class="hall-seat" data-row="5" data-seat="2">2</button>
+              <button class="hall-seat" data-row="5" data-seat="3">3</button>
+              <button class="hall-seat" data-row="5" data-seat="4">4</button>
+              <button class="hall-seat" data-row="5" data-seat="5">5</button>
+              <button class="hall-seat" data-row="5" data-seat="6">6</button>
+              <button class="hall-seat" data-row="5" data-seat="7">7</button>
+              <button class="hall-seat" data-row="5" data-seat="8">8</button>
+              <button class="hall-seat" data-row="5" data-seat="9">9</button>
+              <button class="hall-seat" data-row="5" data-seat="10">10</button>
+              5 row
+            </div>
+            <div class="row text-center">
+              6 row
+              <button class="hall-seat" data-row="6" data-seat="1">1</button>
+              <button class="hall-seat" data-row="6" data-seat="2">2</button>
+              <button class="hall-seat" data-row="6" data-seat="3">3</button>
+              <button class="hall-seat" data-row="6" data-seat="4">4</button>
+              <button class="hall-seat" data-row="6" data-seat="5">5</button>
+              <button class="hall-seat" data-row="6" data-seat="6">6</button>
+              <button class="hall-seat" data-row="6" data-seat="7">7</button>
+              <button class="hall-seat" data-row="6" data-seat="8">8</button>
+              <button class="hall-seat" data-row="6" data-seat="9">9</button>
+              <button class="hall-seat" data-row="6" data-seat="10">10</button>
+              6 row
+            </div>
+            <div class="row text-center">
+              7 row
+              <button class="hall-seat" data-row="7" data-seat="1">1</button>
+              <button class="hall-seat" data-row="7" data-seat="2">2</button>
+              <button class="hall-seat" data-row="7" data-seat="3">3</button>
+              <button class="hall-seat" data-row="7" data-seat="4">4</button>
+              <button class="hall-seat" data-row="7" data-seat="5">5</button>
+              <button class="hall-seat" data-row="7" data-seat="6">6</button>
+              <button class="hall-seat" data-row="7" data-seat="7">7</button>
+              <button class="hall-seat" data-row="7" data-seat="8">8</button>
+              <button class="hall-seat" data-row="7" data-seat="9">9</button>
+              <button class="hall-seat" data-row="7" data-seat="10">10</button>
+              7 row
+            </div>
+
+          </div>
+      <div class="sum text-center">
+        <b>Chosen seats:</b>
+        <ol class ="hall-list"></ol>
+        <b>Total: </b> <span class="hall-sum">0</span>
+      </div>
+    </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default js-close-modal" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-success next-modal js-open-book-modal" data-next="bookModal">Next</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="modal fade" id="bookModal" tabindex="-1" role="dialog" aria-labelledby="bookModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="bookModalLabel">Please, enter booking information</h4>
+      </div>
+      <div class="modal-body">
+          <div class="">
+            <div class="row text-center">
+              <h4>Information is confident.</h4>
+              <form class="col-xs-12" id="book-form">
+                <div class="form-group">
+                  <label>Enter your last name:</label>
+                  <input type="text" class="form-control" id="book-name"placeholder="Last name">
+                </div>
+                <div class="form-group">
+                  <label>Enter your email:</label>
+                  <?php
+                    $email = isset($_COOKIE['email']) ? $_COOKIE['email'] : '';
+                  ?>
+                  <input type="email" class="form-control" id="book-email" placeholder="Email" value="<?php echo $email; ?>">
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default js-close-modal" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-success next-modal js-open-result-modal" data-next="resultModal" disabled="disabled">Next</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="resultModal" tabindex="-1" role="dialog" aria-labelledby="resultModalLabel">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title" id="resultModalLabel">Congrats!</h4>
+        </div>
+        <div class="modal-body">
+            <div class="hall">
+              <div class="row text-center">
+                <!-- <h4>Congrats!</h4> -->
+                <p>Booking information have been sent to email: <span id="result-email"><?php echo $email; ?></span></p>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary js-close-modal" data-dismiss="modal">Ok</button>
+          </div>
+        </div>
+      </div>
+    </div>
